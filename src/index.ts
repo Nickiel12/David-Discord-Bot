6ebc5548b8ce2f4faf5c098fefa0a545cb373179
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2020-05-30 12:54:43 -0700

    on ready, implemented

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,8 @@ import * as Discord from "discord.js"
 let config = require("./../discord_auth_key.json")
 let client = new Discord.Client();
 
+client.on("ready", () => {
+    console.log("I'm connected!")
+})
+
 client.login(config.token);
