5c1f2c76ffc20491d187d52f0fe95fefda0a91e5
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2020-05-30 13:17:42 -0700

    got it to compile

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+var Discord = require("discord.js");
+var config = require("./../discord_auth_key.json");
+var client = new Discord.Client();
+client.on("ready", function () {
+    console.log("I'm connected!");
+});
+client.on("message", function (message) {
+    console.log(message.content);
+});
+client.login(config.token);
+//# sourceMappingURL=index.js.map
